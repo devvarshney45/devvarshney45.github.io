ef4f163060f6e0751c25f8859a71baef33ada514
devvarshney45 <varshneydev365@gmail.com>
2025-08-12 05:40:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var  tyoed=new Typed(".text",{
+/*var  tyoed=new Typed(".text",{
     strings:["frontend Developer","Problem Solver","Editor"],
     typeSpeed:100,
     backSpeed:100,
@@ -6,7 +6,8 @@ var  tyoed=new Typed(".text",{
     loop:true
 
 
-});
+});*/
 document.getElementById("menu-icon").addEventListener("click", function () {
   document.getElementById("navbar").classList.toggle("show");
 });
+
